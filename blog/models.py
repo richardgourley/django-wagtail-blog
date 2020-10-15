1ef948535088b9cac0dcf0d87f63e7f8e8477042
Rich <codeleap@outlook.com>
2020-10-15 20:32:55 +0200

    BlogCategory added.

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,25 @@ from json import dumps
 
 # Create your models here.
 
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    icon = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True, 
+        on_delete=models.SET_NULL, related_name="+"
+    )
+
+    panels = [
+        FieldPanel('name'),
+        ImageChooserPanel('icon')
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'blog categories'
+
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
