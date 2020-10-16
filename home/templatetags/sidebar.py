d9e3f8cc7707a44b3b8dde45661e96f1c1afffe2
Rich <codeleap@outlook.com>
2020-10-16 15:50:01 +0200

    Sidebar template tag added

--- home/templatetags/sidebar.py
+++ home/templatetags/sidebar.py
@@ -1,5 +1,11 @@
 from django import template
-from wagtail.core.models import Site
+from blog.models import BlogPage
 
 register = template.Library()
 
+@register.inclusion_tag("tags/sidebar.html")
+def get_sidebar():
+	blogpages = BlogPage.objects.live().order_by('-first_published_at')
+	return {
+	    "blogpages":blogpages,
+	}
