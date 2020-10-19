c51d83ce365bc68e979a39d891ed7dad2c9be514
Rich <codeleap@outlook.com>
2020-10-19 20:34:09 +0200

    Casing on blogpages changed to blogPages.

--- home/templatetags/sidebar.py
+++ home/templatetags/sidebar.py
@@ -5,9 +5,9 @@ register = template.Library()
 
 @register.inclusion_tag("tags/sidebar.html")
 def get_sidebar():
-	blogpages = BlogPage.objects.live().order_by('-first_published_at')
+	blogPages = BlogPage.objects.live().order_by('-first_published_at')
 	blogTags = BlogPageTag.objects.all()
 	return {
-	    "blogpages":blogpages,
+	    "blogPages":blogPages,
 	    "blogTags":blogTags,
 	}
