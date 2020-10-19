96106fdd25c91e8d91213536c716a7426e9ca098
Rich <codeleap@outlook.com>
2020-10-19 20:21:56 +0200

    Tags added to our sidebar template tag context.

--- home/templatetags/sidebar.py
+++ home/templatetags/sidebar.py
@@ -1,11 +1,13 @@
 from django import template
-from blog.models import BlogPage
+from blog.models import BlogPage, BlogPageTag
 
 register = template.Library()
 
 @register.inclusion_tag("tags/sidebar.html")
 def get_sidebar():
 	blogpages = BlogPage.objects.live().order_by('-first_published_at')
+	blogTags = BlogPageTag.objects.all()
 	return {
 	    "blogpages":blogpages,
+	    "blogTags":blogTags,
 	}
