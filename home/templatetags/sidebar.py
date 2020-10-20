54788571d7ea06d106939ba13c94a026527f30cc
Rich <codeleap@outlook.com>
2020-10-20 18:28:28 +0200

    Tags retrieved from blog pages and returned to our tag template.

--- home/templatetags/sidebar.py
+++ home/templatetags/sidebar.py
@@ -1,13 +1,27 @@
 from django import template
-from blog.models import BlogPage, BlogPageTag
+from blog.models import BlogPage
 
 register = template.Library()
 
+'''
+We can call {% load sibebar %} in other pages(the name of this file) to call this template tag
+After loading, we can call {% get_sidebar %} corresponding to function below..
+.. which passes the data to and displays "tags/sidebar.html"
+'''
 @register.inclusion_tag("tags/sidebar.html")
 def get_sidebar():
-	blogPages = BlogPage.objects.live().order_by('-first_published_at')
-	blogTags = BlogPageTag.objects.all()
-	return {
-	    "blogPages":blogPages,
-	    "blogTags":blogTags,
-	}
+    '''
+    Gets all blog pages and 'unique' tags used with our blog pages...
+    which we use in our sidebar menu - "tags/sidebar.html"
+    '''
+    blog_tags = []
+    blog_pages = BlogPage.objects.live().order_by('-first_published_at')
+    for page in blog_pages:
+        for tag in page.tags.all():
+            if not tag in blog_tags:
+                blog_tags.append(tag)
+    
+    return {
+        "blog_pages":blog_pages,
+        "blog_tags":blog_tags,
+    }
