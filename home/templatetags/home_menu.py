be3b2375e7b9b56d44b3097984bd7a97672a889f
Rich <codeleap@outlook.com>
2020-10-20 18:29:16 +0200

    Variable naming updated.

--- home/templatetags/home_menu.py
+++ home/templatetags/home_menu.py
@@ -2,12 +2,16 @@ from django import template
 from wagtail.core.models import Site
 
 register = template.Library()
-
+'''
+See base.html to see where we call {% load home_menu %} (the name of this file) to load this template tag
+After loading we can call {% get_menu %} (function below)
+which then passes data to and display "tags/navbar.html"
+'''
 @register.inclusion_tag("tags/navbar.html")
 def get_menu():
 	site = Site.objects.get(is_default_site=True)
 	home_page = site.root_page
-	pages = home_page.get_children().live().in_menu()
+	pages = homePage.get_children().live().in_menu()
 	return {
 	    "home_page":home_page,
 	    "pages":pages,
