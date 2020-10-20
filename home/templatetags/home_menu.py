9da32b320307af095dfcc78d97ad14ea65cd3aa3
Rich <codeleap@outlook.com>
2020-10-20 18:54:38 +0200

    Typo fixed

--- home/templatetags/home_menu.py
+++ home/templatetags/home_menu.py
@@ -11,7 +11,7 @@ which then passes data to and display "tags/navbar.html"
 def get_menu():
 	site = Site.objects.get(is_default_site=True)
 	home_page = site.root_page
-	pages = homePage.get_children().live().in_menu()
+	pages = home_page.get_children().live().in_menu()
 	return {
 	    "home_page":home_page,
 	    "pages":pages,
